b7d87831020435159af23a42d3f6a6e35e937086
oreshkin.andrey8 <oreshkin.andrey8@rwb.ru>
2026-04-16 22:59:54 +0300

    First

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
 
+
         }
     }
 }
