c613e7ab26eedd3e56fc039c13a1c9dbbb5bd9e3
The_araspel <a_reshkin@inbox.ru>
2026-04-29 23:53:33 +0300

    2

--- src/main/java/Practice/Task1.java
+++ src/main/java/Practice/Task1.java
@@ -2,7 +2,13 @@ package Practice;
 
 public class Task1 {
     public static void main(String[] args) {
-        String message = "20Ivan";
-        System.out.println(message);
+        String name1 = "Ivan";
+        int num1 = 200;
+        int num2 = 500;
+        String name2 = "Olga";
+        System.out.println(name1);
+        System.out.println(name2);
+        System.out.println(num1);
+        System.out.println(num2);
     }
 }
