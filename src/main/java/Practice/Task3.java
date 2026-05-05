bbf1c02490af0457615a81e270723926834f1814
The_araspel <a_reshkin@inbox.ru>
2026-05-05 23:25:57 +0300

    Practice scanner int

---
+++ src/main/java/Practice/Task3.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int result = a + b;
+        System.out.println(result);
+    }
+}
