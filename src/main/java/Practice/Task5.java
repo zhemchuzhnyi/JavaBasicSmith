80eef406217dec8f7b44816ffa01e6e11fae648f
The_araspel <a_reshkin@inbox.ru>
2026-05-07 23:12:09 +0300

    Practice scanner min max math

---
+++ src/main/java/Practice/Task5.java
@@ -0,0 +1,20 @@
+package Practice;
+
+// Напишите код, который принимает от пользователя три числа
+// (каждое с новой строки, тип данных int) и выводит на печать максимальное из них (тип данных int).
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+       // Scanner input = new Scanner(System.in);
+        int a = 4;
+        int b = 5;
+        int c = 9;
+
+        int d = Math.max(a, b);
+        int v = Math.min(a, c);
+        System.out.println(d);
+        System.out.println(v);
+    }
+}
