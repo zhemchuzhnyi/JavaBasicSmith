56a81a2011e2e4b8b01779e5aae15c34e4ca21a8
The_araspel <a_reshkin@inbox.ru>
2026-05-07 23:38:01 +0300

    Practice scanner  math

--- src/main/java/Practice/Task8.java
+++ src/main/java/Practice/Task8.java
@@ -6,7 +6,7 @@ public class Task8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int days = sc.nextInt();
-        int sec = days * 24 * 60 * 60 * 1000;
+        int sec = days * 24 * 60 * 60;
         System.out.println(sec);
     }
 }
