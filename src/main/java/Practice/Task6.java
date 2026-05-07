ce50146b78647cd1b45bdf18c6c417178511b076
The_araspel <a_reshkin@inbox.ru>
2026-05-07 23:25:46 +0300

    Practice scanner float math

---
+++ src/main/java/Practice/Task6.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = Math.abs(a);
+        System.out.println(b);
+    }
+}
