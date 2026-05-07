2cae15321716a92b9ba74d84857140fe601ec17c
The_araspel <a_reshkin@inbox.ru>
2026-05-07 23:31:10 +0300

    Practice scanner float math

---
+++ src/main/java/Practice/Task7.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int time = sc.nextInt();
+
+        int hours = time / 60;
+        System.out.println(hours);
+    }
+}
