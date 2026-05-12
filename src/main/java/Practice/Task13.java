5483608d7e5e8cd4362e0b2b7989832b09e6b528
The_araspel <a_reshkin@inbox.ru>
2026-05-13 00:57:31 +0300

    Practice scanner  String

---
+++ src/main/java/Practice/Task13.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Task13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int year = sc.nextInt();
+        int result = 2026 - year;
+        String message = String.format("Фильму %s в 2026 году исполнится %d", name, result);
+        System.out.println(message);
+    }
+}
