69e7572eef7ac9f4b224ef5ba91f56ba1017ae5c
The_araspel <a_reshkin@inbox.ru>
2026-05-13 00:33:45 +0300

    Practice scanner  String

---
+++ src/main/java/Practice/Task11.java
@@ -0,0 +1,11 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        System.out.println(text.length());
+    }
+}
