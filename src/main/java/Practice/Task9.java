fca1872aadfc0e60b83a39ce17940728b23afa74
The_araspel <a_reshkin@inbox.ru>
2026-05-12 23:26:04 +0300

    Practice scanner  String

---
+++ src/main/java/Practice/Task9.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String first = sc.nextLine();
+        String second = sc.nextLine();
+
+    }
+}
