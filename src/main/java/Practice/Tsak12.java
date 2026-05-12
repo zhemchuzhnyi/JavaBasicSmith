543f596ef58f2d2e4ab2f77650c52b6e94b6a8b8
The_araspel <a_reshkin@inbox.ru>
2026-05-13 00:37:32 +0300

    Practice scanner  String

---
+++ src/main/java/Practice/Tsak12.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Tsak12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String firstText = sc.nextLine();
+        String secondText = sc.nextLine();
+        System.out.println(firstText.toUpperCase());
+        System.out.println(secondText.toLowerCase());
+    }
+}
