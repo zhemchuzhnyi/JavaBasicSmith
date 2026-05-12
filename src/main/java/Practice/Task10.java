a280685836568e39b4dadfc4a43835038bcb2194
The_araspel <a_reshkin@inbox.ru>
2026-05-12 23:37:43 +0300

    Practice scanner  String

--- src/main/java/Practice/Task10.java
+++ src/main/java/Practice/Task10.java
@@ -6,7 +6,11 @@ public class Task10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word1 = sc.nextLine();
-        String word2 = sc.nextLine();
+
+        char first = word1.charAt(0);
+        char second = word1.charAt(5);
+
+        System.out.println(first + second);
 
 
     }
