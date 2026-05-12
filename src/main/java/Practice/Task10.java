3e05f05852d93fa1680a4151b979b4124ad19163
The_araspel <a_reshkin@inbox.ru>
2026-05-12 23:32:23 +0300

    Practice scanner  String

---
+++ src/main/java/Practice/Task10.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Task10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word1 = sc.nextLine();
+        String word2 = sc.nextLine();
+
+
+    }
+}
