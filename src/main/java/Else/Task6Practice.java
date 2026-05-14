58e14e1cbd626fecfeb06478b83c99865c558177
The_araspel <a_reshkin@inbox.ru>
2026-05-15 00:23:25 +0300

    Practice else

---
+++ src/main/java/Else/Task6Practice.java
@@ -0,0 +1,18 @@
+package Else;
+
+import java.util.Scanner;
+
+public class Task6Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = (n + m);
+
+        if (k > 160) {
+            System.out.println("Поступил");
+        } else {
+            System.out.println("УВЫ...");
+        }
+    }
+}
