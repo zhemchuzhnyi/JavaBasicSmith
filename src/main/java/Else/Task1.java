e075bd7aba37a07d289d83c267d5ca2c9d5929fd
The_araspel <a_reshkin@inbox.ru>
2026-05-14 23:26:55 +0300

    Practice else

--- src/main/java/Else/Task1.java
+++ src/main/java/Else/Task1.java
@@ -2,6 +2,12 @@ package Else;
 
 public class Task1 {
     public static void main(String[] args) {
+        int variable = 5;
+         if (variable == 5) {
+             System.out.println("The number is 5");
+         } else {
+             System.out.println("The number is not 5");
+         }
 
     }
 }
