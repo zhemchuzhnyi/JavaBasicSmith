8a7a081fdf863228e0275379eb0ea2355e8c8fc1
The_araspel <a_reshkin@inbox.ru>
2026-05-14 23:30:02 +0300

    Practice else

---
+++ src/main/java/Else/Task2.java
@@ -0,0 +1,13 @@
+package Else;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your city");
+
+        String message = sc.nextLine();
+
+    }
+}
