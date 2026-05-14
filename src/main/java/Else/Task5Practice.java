ec1b50af3f4e1fd3f46d7151590d6fcef64b2937
The_araspel <a_reshkin@inbox.ru>
2026-05-15 00:17:22 +0300

    Practice else

---
+++ src/main/java/Else/Task5Practice.java
@@ -0,0 +1,16 @@
+package Else;
+
+import java.util.Scanner;
+
+public class Task5Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (num % 2 == 0 ) {
+            System.out.println("Четное");
+        } else {
+            System.out.println("Нечетное");
+        }
+    }
+}
