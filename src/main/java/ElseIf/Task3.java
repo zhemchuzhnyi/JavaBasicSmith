e420e5506a31358f7082f1797aa03558860ae06e
The_araspel <a_reshkin@inbox.ru>
2026-05-16 20:03:57 +0300

    Practice Else If

---
+++ src/main/java/ElseIf/Task3.java
@@ -0,0 +1,22 @@
+package ElseIf;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Yes or Not=: ");
+        String answer = sc.nextLine();
+        if (answer.equals("Yes")) {
+            System.out.println("Yes? or maybe No?: ");
+            String answer1 = sc.nextLine();
+            if (answer1.equals("Yes")) {
+                System.out.println("Yes means Yes");
+            } else {
+                System.out.println("Changed your mind");
+            }
+        } else {
+            System.out.println("You selected No");
+        }
+    }
+}
