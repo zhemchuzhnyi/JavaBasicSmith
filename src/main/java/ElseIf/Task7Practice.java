e92411eb72175ee3a2f80d97867275205a47a260
The_araspel <a_reshkin@inbox.ru>
2026-05-18 23:57:59 +0300

    Practice Else If && ||

---
+++ src/main/java/ElseIf/Task7Practice.java
@@ -0,0 +1,18 @@
+package ElseIf;
+
+import java.util.Scanner;
+
+public class Task7Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (num < 18) {
+            System.out.println("Early");
+        } else if (num >= 18 && num >= 28 ) {
+            System.out.println("Coming");
+        } else {
+            System.out.println("Late");
+        }
+    }
+}
