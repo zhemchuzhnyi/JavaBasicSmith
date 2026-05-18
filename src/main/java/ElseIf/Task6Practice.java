4f28f56f11e3a9a780cef99b34ef53a25714167d
The_araspel <a_reshkin@inbox.ru>
2026-05-18 23:26:24 +0300

    Practice Else If && ||

--- src/main/java/ElseIf/Task6Practice.java
+++ src/main/java/ElseIf/Task6Practice.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Task6Practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println("Positive"); {
+                if (num == 0) {
+                    System.out.println("Zero");
+                }
+            }
+        } else {
+            System.out.println("Negative");
+        }
 
     }
 }
