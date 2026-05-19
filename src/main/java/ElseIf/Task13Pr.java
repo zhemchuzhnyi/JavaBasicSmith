b6419a001ae40618a90ab07f94e2f85e7fc69ed0
The_araspel <a_reshkin@inbox.ru>
2026-05-20 01:14:58 +0300

    Practice Else If && ||

--- src/main/java/ElseIf/Task13Pr.java
+++ src/main/java/ElseIf/Task13Pr.java
@@ -5,16 +5,34 @@ import java.util.Scanner;
 public class Task13Pr {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        double c = sc.nextInt();
+
+        double min, max;
+
+        if (a <= b && a <= c) {
+            min = a;
+        } else if (b <= a && b <= c) {
+            min = b;
+        } else {
+            min = c;
+
+        }
 
         if (a >= b && a >= c) {
-            System.out.println(a);
+            max = a;
         } else if (b >= a && b >= c) {
-            System.out.println(b);
+            max = b;
         } else {
-            System.out.println(c);
+            max = c;
+        }
+        double diff = (max - min) / min * 100;
+
+        if (diff < 20) {
+            System.out.println("Все молодцы");
+        } else {
+            System.out.println("Кто - то халтурит");
         }
     }
 }
