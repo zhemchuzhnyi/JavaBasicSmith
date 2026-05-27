c6d6b6dc7a09090fbcaaee8910a4f959aed4bf8e
The_araspel <a_reshkin@inbox.ru>
2026-05-27 23:38:17 +0300

    Practice Else If && ||

---
+++ src/main/java/ElseIf/Task14Pr.java
@@ -0,0 +1,13 @@
+package ElseIf;
+
+import java.util.Scanner;
+
+public class Task14Pr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+    }
+}
