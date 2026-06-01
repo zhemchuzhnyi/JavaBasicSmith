6d56241b4957d2c5d8937d3eac01b9ae6af3767c
The_araspel <a_reshkin@inbox.ru>
2026-06-02 00:37:55 +0300

    Practice TryCatch

---
+++ src/main/java/TryCatch/Task2Practice.java
@@ -0,0 +1,23 @@
+package TryCatch;
+
+import java.util.Scanner;
+
+public class Task2Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println("Enter the number: from 1 to 3");
+
+        try {
+            int num = Integer.parseInt(input);
+
+            if (num < 1 || num > 3) {
+                System.out.println(num);
+            } else {
+                System.out.println("Некорректный ввод");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Некорректный ввод");
+        }
+    }
+}
