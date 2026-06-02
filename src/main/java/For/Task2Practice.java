352ccb8465613a32df65331098e7e94a2189b027
The_araspel <a_reshkin@inbox.ru>
2026-06-03 01:04:34 +0300

    Practice For

---
+++ src/main/java/For/Task2Practice.java
@@ -0,0 +1,9 @@
+package For;
+
+public class Task2Practice {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i * i);
+        }
+    }
+}
