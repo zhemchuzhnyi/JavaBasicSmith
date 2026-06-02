0aceb826a1e182618e706e5c84ff1d5583bbc254
The_araspel <a_reshkin@inbox.ru>
2026-06-03 00:48:50 +0300

    Practice For

--- src/main/java/For/Task1Practice.java
+++ src/main/java/For/Task1Practice.java
@@ -9,5 +9,10 @@ public class Task1Practice {
         for (int i = 10; i > 0; i -= 2 ) {
             System.out.println("Current counter i: " + i);
         }
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) break;
+            System.out.println("Iteration: " + 1);
+        }
     }
 }
