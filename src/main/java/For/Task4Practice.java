fcb86a4837a2c7ce25c858831bd743e06eac7431
The_araspel <a_reshkin@inbox.ru>
2026-06-04 23:28:44 +0300

    Practice For

---
+++ src/main/java/For/Task4Practice.java
@@ -0,0 +1,15 @@
+package For;
+
+public class Task4Practice {
+    public static void main(String[] args) {
+        String text = "Hello, World";
+        String translate = "Привет, Мир";
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(text);
+        }
+        for (int i = 0; i < 7; i++) {
+            System.out.println(translate);
+        }
+    }
+}
