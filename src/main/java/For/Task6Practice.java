cc19e788c0ae4a2bee61c7d3c66b8ebfaed5945b
The_araspel <a_reshkin@inbox.ru>
2026-06-15 22:59:24 +0300

    Practice For

--- src/main/java/For/Task6Practice.java
+++ src/main/java/For/Task6Practice.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Task6Practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String message = sc.nextLine();
+        int n = sc.nextInt();
+
+
 
     }
 }
