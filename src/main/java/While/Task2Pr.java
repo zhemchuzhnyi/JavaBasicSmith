c085fd77aca56d90dcfa7c92de9617907d62167c
The_araspel <a_reshkin@inbox.ru>
2026-06-17 23:19:32 +0300

    Practice While

---
+++ src/main/java/While/Task2Pr.java
@@ -0,0 +1,13 @@
+package While;
+
+import java.util.Scanner;
+
+public class Task2Pr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int finish = sc.nextInt();
+
+
+    }
+}
