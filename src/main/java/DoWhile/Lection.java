026cd3d2374f2dba1260f5d71bc4d1422709ed4a
The_araspel <a_reshkin@inbox.ru>
2026-06-17 23:57:10 +0300

    Practice Do-While

--- src/main/java/DoWhile/Lection.java
+++ src/main/java/DoWhile/Lection.java
@@ -2,7 +2,7 @@ package DoWhile;
 
 public class Lection {
     public static void main(String[] args) {
-        int count = 5;
+        int count = 15;
 
         do {
             System.out.println("Iteration: " + count);
