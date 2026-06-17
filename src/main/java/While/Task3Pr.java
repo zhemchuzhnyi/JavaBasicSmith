9b2df13f85d7d099a74d67130d6bb0b128d20170
The_araspel <a_reshkin@inbox.ru>
2026-06-17 23:32:10 +0300

    Practice While

--- src/main/java/While/Task3Pr.java
+++ src/main/java/While/Task3Pr.java
@@ -5,5 +5,9 @@ import java.util.Scanner;
 public class Task3Pr {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        String greet = sc.nextLine();
+        String word = sc.nextLine();
+
     }
 }
