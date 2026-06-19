5d2b8a8628d695d0e9a272bb04a3f337d82de452
The_araspel <a_reshkin@inbox.ru>
2026-06-19 23:24:12 +0300

    Practice Do-While

---
+++ src/main/java/DoWhile/Task2Pr.java
@@ -0,0 +1,21 @@
+package DoWhile;
+
+import java.util.Scanner;
+
+public class Task2Pr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int num = 1;
+
+        if (n >= num) {
+            do {
+                sum += num;
+                num++;
+            } while (num <= n);
+        }
+        System.out.println(sum);
+
+    }
+}
