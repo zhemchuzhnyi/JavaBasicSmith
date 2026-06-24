ab6638a2cc89211f1d73b9ad43d5020de1ab2de7
The_araspel <a_reshkin@inbox.ru>
2026-06-25 01:15:40 +0300

    Practice File

--- src/main/java/WorkWithFile/Task1.java
+++ src/main/java/WorkWithFile/Task1.java
@@ -26,7 +26,8 @@ public class Task1 {
 //        }
 
         try (BufferedReader reader = new BufferedReader(new FileReader("src/file.txt"))) {
-            String line = reader.readLine();
+            String line;
+            while ((line = reader.readLine()) != null) {}
             System.out.println(line);
         }catch (IOException e) {
             System.out.println("Error");
